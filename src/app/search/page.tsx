28c209694598210bb8cef5c4086366f5e1ac4e83
kko3093 <kko3093@gmail.com>
2025-08-20 22:26:46 +0900

    [ feat ] 검색 결과 리스트 정렬 기능 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,16 +1,45 @@
-import type { Metadata } from 'next';
+'use client';
 
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+// components
 import BookList from './_components/BookList';
+import SortAndFilter from './_components/SortAndFilter';
 
-export const metadata: Metadata = {
-    title: '교보문고 검색',
-    description: '교보문고 검색 페이지',
-};
+// "use client" 사용 위해 임시 주석처리
+// export const metadata: Metadata = {
+//     title: 'Search',
+//     description: 'Search for books',
+// };
 
 export default function SearchPage() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    // URL 파라미터 초기값 가져오기
+    const initialSort =
+        (searchParams.get('sort') as 'latest' | 'title' | 'price_asc' | 'price_desc') || 'latest';
+    const initialPageSize = (Number(searchParams.get('len')) as 3 | 5 | 10 | 20) || 20;
+
+    const [sort, setSort] = useState(initialSort);
+    const [pageSize, setPageSize] = useState(initialPageSize);
+
+    // sort, pageSize 변경되면 URL 업데이트
+    useEffect(() => {
+        const keyword = searchParams.get('keyword') || '';
+        const page = searchParams.get('page') || 1;
+        router.push(`?keyword=${keyword}&page=${page}&sort=${sort}&len=${pageSize}`);
+    }, [sort, pageSize]);
+
     return (
         <div className='p-6'>
-            <BookList />
+            <SortAndFilter
+                sort={sort}
+                onChangeSort={setSort}
+                pageSize={pageSize}
+                onChangePageSize={setPageSize}
+            />
+            <BookList sort={sort} pageSize={pageSize} />
         </div>
     );
 }
